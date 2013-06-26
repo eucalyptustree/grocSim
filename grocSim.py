db07938aa01595b964f542b1a5e5c0246fc636fb
Yuki Yamada <7drqzgx6ow6s@gmail.com>
2013-06-25 22:41:35 -0400

    started moxing on shopper class

--- grocSim.py
+++ grocSim.py
@@ -2,3 +2,21 @@
 Grocery store checkout line simulation.
 """
 
+import random
+
+class shopper:
+    def __init__(self):
+        self.cart = random.randint(1,24)
+        self.waitTime = 0
+        self.inLine = True
+    def tick(self):
+        self.waitTime +=1
+    def checkout(self):
+        
+            
+
+class checkoutLine:
+    def __init__(self):
+        pass
+    
+    
